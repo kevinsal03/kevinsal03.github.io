c4d3951b6344578aae4c15ec3eea77cd4a37ad4a
Kevin Salvatorelli <kevinsal03@users.noreply.github.com>
2017-10-29 18:06:45 -0400

    Update offline_check.js

--- res/ls/offline_check.js
+++ res/ls/offline_check.js
@@ -1,20 +1,15 @@
 var player = videojs('live-video');
+var last_value = 0;
+var offline_test = 0;
 player.on('timeupdate', function() {
-    //$("#sobytes-liveplayer-offline-message").hide();
     if (player.currentTime() === last_value) {
         if (offline_test > 3) {
             player.userActive(true);
             $("#video .vjs-live-display").text("OFFLINE");
             $("#video .vjs-remaining-time-display").text("OFFLINE");
-            //$("#sobytes-live-image").attr("src", "/images/circle_red.png");
             offline_test = 0;
         }
         offline_test++;
-    } /*else {
-        offline_test = 0;
-        $("#video .vjs-live-display").text("LIVE");
-        $("#video .vjs-remaining-time-display").text("LIVE");
-        $("#sobytes-live-image").attr("src", "/images/circle_green.png");
-    } */
+    }
     last_value = player.currentTime();
 });
